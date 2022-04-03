56dc594c00bac9990a69b6d7550bd6b96b6c22fc
lkx <1336354338@qq.com>
2022-04-03 15:57:14 +0800

    feat(@snaphooks/xingyun-lowcode-editor): 添加图层树

--- src/components/toolbox/components/LayerTree/index.tsx
+++ src/components/toolbox/components/LayerTree/index.tsx
@@ -1,11 +1,13 @@
 import { Divider } from '@arco-design/web-react';
 import * as S from './styled';
+import { Layers } from '@craftjs/layers';
 
 const LayerTree = () => {
   return (
     <S.LayerTreeContainer>
       <S.ToolTitle>图层树</S.ToolTitle>
       <Divider />
+      <Layers expandRootOnLoad />
     </S.LayerTreeContainer>
   );
 };
