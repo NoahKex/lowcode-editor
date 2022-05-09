ad34e5f7d9fce046bee1bf8d5df69a51fa20e4f4
lkx <1336354338@qq.com>
2022-05-10 00:54:01 +0800

    feat(@snaphooks/xingyun-lowcode-editor): 完善保存功能,添加数据导入功能C

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,40 +1,66 @@
-import { Switch, Button, Message } from '@arco-design/web-react';
+import { Button, Message, Modal, Input } from '@arco-design/web-react';
 import * as S from './styled';
 import { useEditor } from '@craftjs/core';
 import { IconRedo, IconUndo } from '@arco-design/web-react/icon';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from 'copy-to-clipboard';
 import lz from 'lzutf8';
+import { useState } from 'react';
+
+const { TextArea } = Input;
 
 const TopBar = () => {
   const { actions, query } = useEditor();
-
+  const [loadDataVisible, setLoadDataVisible] = useState(false);
+  const [inputData, setInputData] = useState('');
   return (
-    <S.TopBarContainer>
-      <S.LogoContainer >
-        <S.Logo src="src/assets/logo.png" />
-        <S.Title>行云低代码设计器</S.Title>
-      </S.LogoContainer>
-      <S.HistoryContainer >
-        <Button type="secondary" icon={<IconUndo />} />
-        <Button type="secondary" icon={<IconRedo />} />
-      </S.HistoryContainer>
-      <S.OperationContainer >
-        <CopyToClipboard
-          text={lz.encodeBase64(lz.compress(query.serialize()))}
-          onCopy={(_, result) => {
-            if (result) {
-              Message.success('数据已成功保存到剪贴板！');
-            } else {
-              Message.error('数据保存到剪贴板失败！');
-            }
+    <>
+      <S.TopBarContainer>
+        <S.LogoContainer>
+          <S.Logo src="src/assets/logo.png" />
+          <S.Title>行云低代码设计器</S.Title>
+        </S.LogoContainer>
+        <S.HistoryContainer>
+          <Button type="secondary" icon={<IconUndo />} />
+          <Button type="secondary" icon={<IconRedo />} />
+        </S.HistoryContainer>
+        <S.OperationContainer>
+          <Button
+            type="primary"
+            style={{ marginRight: '10px' }}
+            onClick={() => {
+              const copyStatus = copy(lz.encodeBase64(lz.compress(query.serialize())));
+              if (copyStatus) {
+                Message.success('数据已成功保存到剪贴板！');
+              } else {
+                Message.error('数据保存到剪贴板失败！');
+              }
+            }}
+          >保存
+          </Button>
+          <Button type="primary" style={{ marginRight: '5px' }}>预览</Button>
+          <Button style={{ marginRight: '5px' }} onClick={() => setLoadDataVisible(true)}>导入数据</Button>
+        </S.OperationContainer>
+      </S.TopBarContainer>
+      <Modal
+        title="导入数据"
+        visible={loadDataVisible}
+        onOk={() => {
+          setLoadDataVisible(false);
+          actions.deserialize(lz.decompress(lz.decodeBase64(inputData)));
+          Message.success('数据已成功导入设计器！');
+        }}
+        onCancel={() => setLoadDataVisible(false)}
+      >
+        <TextArea
+          value={inputData}
+          placeholder="请粘贴序列化数据到文本框中"
+          style={{ minHeight: '100px', width: '100%' }}
+          onChange={(val) => {
+            setInputData(val);
           }}
-        >
-          <Button type="primary" style={{ marginRight: '10px' }}>保存</Button>
-        </CopyToClipboard>
-        <Button type="primary" style={{ marginRight: '5px' }}>预览</Button>
-        <Button style={{ marginRight: '5px' }}>导入数据</Button>
-      </S.OperationContainer>
-    </S.TopBarContainer>
+        />
+      </Modal>
+    </>
   );
 };
 
