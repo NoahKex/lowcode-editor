0808d2488cebc021496ebb908395fbcb6c6179b8
lkx <1336354338@qq.com>
2022-05-09 23:08:45 +0800

    feat(@snaphooks/xingyun-lowcode-editor): 添加保存功能

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,23 +1,15 @@
-import { Switch, Button } from '@arco-design/web-react';
+import { Switch, Button, Message } from '@arco-design/web-react';
 import * as S from './styled';
 import { useEditor } from '@craftjs/core';
 import { IconRedo, IconUndo } from '@arco-design/web-react/icon';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import lz from 'lzutf8';
 
 const TopBar = () => {
-  const { actions, enabled } = useEditor((state) => ({
-    enabled: state.options.enabled,
-  }));
+  const { actions, query } = useEditor();
 
   return (
     <S.TopBarContainer>
-      {/* 是否编辑<Switch
-        checked={enabled}
-        onChange={(val) => {
-          actions.setOptions((options) => { options.enabled = val; });
-        }}
-      />
-      <Button>保存json</Button>
-      <Button>导入json</Button> */}
       <S.LogoContainer >
         <S.Logo src="src/assets/logo.png" />
         <S.Title>行云低代码设计器</S.Title>
@@ -27,7 +19,18 @@ const TopBar = () => {
         <Button type="secondary" icon={<IconRedo />} />
       </S.HistoryContainer>
       <S.OperationContainer >
-        <Button type="primary" style={{ marginRight: '10px' }}>保存</Button>
+        <CopyToClipboard
+          text={lz.encodeBase64(lz.compress(query.serialize()))}
+          onCopy={(_, result) => {
+            if (result) {
+              Message.success('数据已成功保存到剪贴板！');
+            } else {
+              Message.error('数据保存到剪贴板失败！');
+            }
+          }}
+        >
+          <Button type="primary" style={{ marginRight: '10px' }}>保存</Button>
+        </CopyToClipboard>
         <Button type="primary" style={{ marginRight: '5px' }}>预览</Button>
         <Button style={{ marginRight: '5px' }}>导入数据</Button>
       </S.OperationContainer>
